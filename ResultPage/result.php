45b686a1a148cb94a6f8a4b8cf24dd3976124688
Karolina <karolina.limanowska@gmail.com>
2024-02-14 14:33:05 +0100

    working on a result page

---
+++ ResultPage/result.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>SearchPage</title>
+    </head>
+
+    <body>
+        <header>
+            <div class="mobile">
+                <nav>
+                    <h2>Name of the area</h2>
+                    <p>longtitute</p>
+                    <p>latitude</p>
+                    <div class="menu"></div>
+                </nav>
+            </div>
+        </header>
+
+    </body>
+</html>
