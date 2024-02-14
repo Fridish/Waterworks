d0552867006ec56ff6339540f8bac51bcbd5a489
Karolina <karolina.limanowska@gmail.com>
2024-02-14 14:51:25 +0100

    adding buttons

--- ResultPage/result.php
+++ ResultPage/result.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html> 
-<html>
+<html lang="sve">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>SearchPage</title>
     </head>
 
@@ -10,9 +11,15 @@
             <div class="mobile">
                 <nav>
                     <h2>Name of the area</h2>
-                    <p>longtitute</p>
-                    <p>latitude</p>
-                    <div class="menu"></div>
+                    <p>Longtitute</p>
+                    <p>Latitude</p>
+                    <div class="menu">
+                    <button class="btn-menu">One</button>
+                    <button class="btn-menu">Two</button>
+                    <button class="btn-menu">Three</button>
+                    <button class="btn-menu">Four</button>
+                    <button class="btn-menu">Five</button>
+                    </div>
                 </nav>
             </div>
         </header>
