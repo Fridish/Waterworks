e3e45fe990462cf8caf7c1d954083f172727cfac
Frida Fernhede <fernhedefrida@gmail.com>
2024-02-14 16:34:09 +0100

    Fixed the dropdown and added header/footer code
    Also added some temporary ugly styling

--- Components/header.php
+++ Components/header.php
@@ -4,4 +4,5 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="/../Styles/styles.css" />
+</head>
