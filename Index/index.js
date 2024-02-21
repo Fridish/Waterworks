b5a3814862c578a2f88a5743e4534cee0d664eb3
MylAnt95 <anton.myllykoski95@gmail.com>
2024-02-21 10:07:27 +0100

    added colors, fonts and a new dropdownmenu

--- Index/index.js
+++ Index/index.js
@@ -12,18 +12,3 @@ function toggleContent() {
     this.children[1].classList.add("active");
   }
 } */
-
-
-// Drop down menu
-const hamburger = document.querySelector(".hamburger");
-const dropMenu = document.querySelector(".drop-menu");
-const cancel = document.querySelector(".cancel img");
-
-hamburger.addEventListener("click", () => {
-    dropMenu.style.display = "flex";
-    dropMenu.style.animation = "dropDown 300ms ease-in-out forwards";
-    dropMenu.style.transformOrigin = "top center";
-    cancel.addEventListener("click", () => {
-        dropMenu.style.display = "none";
-    })
-});
