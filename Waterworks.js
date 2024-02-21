58b3ef88717a07febbb2f06b8e86b4a192741540
Cappe <casper@sundegard.st>
2024-02-21 17:24:03 +0100

    Today's Level Chart created

--- Waterworks.js
+++ Waterworks.js
@@ -3,13 +3,17 @@ class Waterworks{
 		return ['Agnesberg', 'Arketjarn', 'Eriksberg', 'Garda', 'Harsjo', 'Kalleredsbacken', 'Landvetter', 'Levgrensvagen', 'Larjean', 'MolndalCentrum', 'Nedsjon', 'Rada', 'Skars led', 'Slussen', 'Stensjon', 'Tingstad', 'Torshamnen'];
 	}
 
-	static async createChart(type, site, param, stDate, edDate){
+	static getSitesSE(){
+		return ['Agnesberg', 'Arketjärn', 'Eriksberg', 'Gårda dämme', 'Härsjo dämme', 'Kålleredsbäcken', 'Landvettersjöns dämme', 'Levgrensvägen', 'Lärjeholm', 'Mölndal C', 'Nedsjöns dämme', 'Rådasjön', 'Skårs led', 'Slussen', 'Stensjö dämme', 'Tingstad', 'Torshamnen'];
+	}
+
+	static async createChart(cont, type, site, stDate, edDate){
 		if (!this.getSites().includes(site)){
 			console.log('Site not found');
 			return;
 		}
 
-		let response = await fetch(`https://data.goteborg.se/RiverService/v1.1/Measurements/0f254316-99ab-4a86-90d4-25438b6822cc/${site}/${param}/${stDate}/${edDate}?format=json`);
+		let response = await fetch(`https://data.goteborg.se/RiverService/v1.1/Measurements/0f254316-99ab-4a86-90d4-25438b6822cc/${site}/Level/${stDate}/${edDate}?format=json`);
 		let fetchData = await response.json();
 
 		let values = [];
@@ -20,20 +24,20 @@ class Waterworks{
 
 		switch (type){
 		case "test":
-			this.chartGen(values);
+			this.chartGen(cont, values);
 			break;
 		case "line":
-			this.chartJS('line', values);
+			this.chartJS(cont, 'line', values, stDate, edDate);
 			break;
 		case "bar":
-			this.chartJS('bar', values);
+			this.chartJS(cont, 'bar', values, stDate, edDate);
 			break;
 		default:
 			console.log('Not a valid chart');
 		}
 	}
 
-	static chartGen(values){
+	static chartGen(cont, values){
 		let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 		let line = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
 
@@ -62,43 +66,93 @@ class Waterworks{
 		});
 
 		svg.appendChild(line);
-		chartCont.appendChild(svg);
+		cont.appendChild(svg);
 	}
 
-	static chartJS(type, values){
-		const canvas = document.createElement('canvas');
+	static chartJS(cont, type, values, stDate, edDate){
+		let canvas = document.createElement('canvas');
 
 		new Chart(canvas, {
 			type: type,
 			data: {
-				labels: this.months({count: values.length}),
+				labels: this.getDateLabels(values.length, stDate, edDate),
 				datasets: [{
     				label: mSite.value+" - Vattennivå",
     				data: values,
     				fill: false,
-    				borderColor: 'rgb(75, 192, 192)',
-    				tension: 0.1
+    				tension: 0
+				}],
+			}
+		});
+
+		cont.appendChild(canvas);
+	}
+
+	static getDateLabels(count, stDate, edDate){
+		const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+		let labels = [];
+
+		let day = +stDate.split('-')[2];
+		let month = +stDate.split('-')[1];
+		let year = +stDate.split('-')[0];
+
+		for (let i = 0; i < count; i++){
+			if (day > monthDays[month-1] + (month == 2) * (year % 4 == 0)){
+				month++;
+				day = 1;
+			}
+			if (month > 12){
+				year++;
+				month = 1;
+			}
+			labels.push(day+"/"+month+" - "+year);
+			day++;
+		}
+		return labels;
+	}
+
+	static async chartToday(cont){
+		let date = new Date();
+		const offset = date.getTimezoneOffset();
+		date = new Date(date.getTime() - (offset*60*1000));
+		let today = date.toISOString().split('T')[0];
+		date = new Date(date.getTime() - 24*60*60*1000);
+		let yesterday = date.toISOString().split('T')[0];
+
+		let values = [];
+
+		for (let i = 0; i < this.getSites().length; i++){
+			let response = await fetch(`https://data.goteborg.se/RiverService/v1.1/Measurements/0f254316-99ab-4a86-90d4-25438b6822cc/${this.getSites()[i]}/Level/${yesterday}/${today}?format=json`);
+			let fetchData = await response.json();
+	
+			fetchData.forEach((index) =>{
+				values.push(index['Value']);
+			});
+		}
+
+		let canvas = document.createElement('canvas');
+
+		new Chart(canvas, {
+			type: 'bar',
+			data: {
+				labels: this.getSitesSE(),
+				datasets: [{
+				    label: "Today's Water Level",
+				    data: values,
+				    fill: false,
+				    tension: 0
 				}]
 			}
 		});
 
-		chartCont.appendChild(canvas);
-	}
-
-	static months(config){
-		let count = config.count;
-		let values = [];
-
-		for (let i = 0; i < count; ++i) {
-			values.push("No Label");
-		}
-
-		return values;
+		cont.appendChild(canvas);
 	}
 }
+
+Waterworks.chartToday(chartCont);
 
 // Waterworks.createChart('Agnesberg', 'Level', '2022-10-14', '2023-10-20');
 
 chartBtn.addEventListener('click', () =>{
-	Waterworks.createChart(chartType.value, mSite.value, 'Level', stDate.value, edDate.value);
+	Waterworks.createChart(chartCont, chartType.value, mSite.value, stDate.value, edDate.value);
 });
