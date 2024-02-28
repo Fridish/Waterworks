86d7351bd80f676fd832710a98b4ecd83cb09a20
MylAnt95 <anton.myllykoski95@gmail.com>
2024-02-28 14:54:19 +0100

    added animation to frontpage

--- Components/footer.php
+++ Components/footer.php
@@ -1,6 +1,7 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="Functions/Waterworks.js"></script>
 <script type="module" src="Functions/index.js"></script>
+<script src="../Index/anime.js"></script>
 </body>
 
 </html>
